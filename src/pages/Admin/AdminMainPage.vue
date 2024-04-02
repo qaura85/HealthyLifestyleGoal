854edaea04c463495877e9065301a69018669334
NikNafis <niknafis96@yahoo.com>
2024-04-02 12:38:22 +0800

    2/4/24 12:28PM

--- src/pages/Admin/AdminMainPage.vue
+++ src/pages/Admin/AdminMainPage.vue
@@ -2,256 +2,232 @@
   <q-page padding class="bg-grey-1">
     <img />
     <div>
-    <q-card class="bg-transparent" flat>
+      <q-card class="bg-transparent" flat>
+        <!-- Title -->
 
-      <!-- Title -->
-
-      <q-card-section align="center" class="title">
-        <h2 class="text-secondary text-h2 q-my-none text-weight-bolder">
+        <q-card-section align="center" class="title">
+          <h2 class="text-secondary text-h2 q-my-none text-weight-bolder">
             <span>Healthy Lifestyle Goals</span>
-        </h2>
-      </q-card-section>
-      <q-separator />
-      <div class="row q-pa-md">
+          </h2>
+        </q-card-section>
+        <q-separator />
+        <div class="row q-pa-md">
+          <div class="col-12 col-md-1 text-transparent">_</div>
 
-        <div class="col-12 col-md-1 text-transparent">
-          _
-        </div>
+          <!-- Today's Goals -->
 
-        <!-- Today's Goals -->
-
-        <div class="col-12 col-md-5">
-          <q-card>
-          <q-card-section class="bgmint text-weight-bolder text-white">
-            <q-avatar square>
-              <img src="~assets/target.png" >
-            </q-avatar>
-            <span class="text-h5 ">Today's Goals</span>
-          </q-card-section>
-          <q-separator class="bg-black" />
-          <q-card-section vertical class="goal">
-            <div class="row">
-              <div class="col-6 q-pa-md">
-                <span flat>Goal 1</span>
-              </div>
-              <div class="col-6 q-pa-md" align="right">
-                <q-checkbox v-model="goal1" color="green" />
-              </div>
-              <div class="col-6 q-pa-md">
-                <span>Goal 2</span>
-              </div>
-              <div class="col-6 q-pa-md" align="right">
-                <q-checkbox v-model="goal2" color="green" />
-              </div>
-              <div class="col-6 q-pa-md">
-                <span>Goal 3</span>
-              </div>
-              <div class="col-6 q-pa-md" align="right">
-                <q-checkbox v-model="goal3" color="green" />
-              </div>
-            </div>
-          <q-separator class="bg-grey" />
-          </q-card-section>
-          <q-card-section class="bg-primary" align="right">
-          <q-btn @click="submitGoal = true" label="Submit" class="bg-secondary text-white"/>
-          </q-card-section>
-        </q-card>
-        </div>
-
-        <div class="col-12 col-md-1 text-transparent">
-          _
-        </div>
-
-        <!-- News -->
-
-        <div class="col-12 col-md-4">
-          <q-card>
-          <q-card-section class="bgmint text-weight-bolder text-white">
-            <q-avatar square>
-              <img src="~assets/news.png" >
-            </q-avatar>
-            <span class="text-h5 ">News</span>
-          </q-card-section>
-          <q-separator class="bg-black" />
-          <q-card-section vertical class="goal">
-            <div class="row">
-              <div class="col-12 q-pa-md">
+          <div class="col-12 col-md-5">
+            <q-card>
+              <q-card-section class="bgmint text-weight-bolder text-white">
                 <q-avatar square>
-                  <img src="~assets/news.png" >
+                  <img src="~assets/target.png" />
                 </q-avatar>
-                  News 1
-              </div>
-              <div class="col-12 q-pa-md">
+                <span class="text-h5">Today's Goals</span>
+              </q-card-section>
+              <q-separator class="bg-black" />
+              <q-card-section vertical class="goal">
+                <div class="row">
+                  <div class="col-6 q-pa-md">
+                    <span flat>Goal 1</span>
+                  </div>
+                  <div class="col-6 q-pa-md" align="right">
+                    <q-checkbox v-model="goal1" color="green" />
+                  </div>
+                  <div class="col-6 q-pa-md">
+                    <span>Goal 2</span>
+                  </div>
+                  <div class="col-6 q-pa-md" align="right">
+                    <q-checkbox v-model="goal2" color="green" />
+                  </div>
+                  <div class="col-6 q-pa-md">
+                    <span>Goal 3</span>
+                  </div>
+                  <div class="col-6 q-pa-md" align="right">
+                    <q-checkbox v-model="goal3" color="green" />
+                  </div>
+                </div>
+                <q-separator class="bg-grey" />
+              </q-card-section>
+              <q-card-section class="bg-primary" align="right">
+                <q-btn
+                  @click="submitGoal = true"
+                  label="Submit"
+                  class="bg-secondary text-white"
+                />
+              </q-card-section>
+            </q-card>
+          </div>
+
+          <div class="col-12 col-md-1 text-transparent">_</div>
+
+          <!-- News -->
+
+          <div class="col-12 col-md-4">
+            <q-card>
+              <q-card-section class="bgmint text-weight-bolder text-white">
                 <q-avatar square>
-                  <img src="~assets/news.png" >
+                  <img src="~assets/news.png" />
                 </q-avatar>
-                  News 2
-              </div>
-              <div class="col-12 q-pa-md">
+                <span class="text-h5">News</span>
+              </q-card-section>
+              <q-separator class="bg-black" />
+              <q-card-section vertical class="goal">
+                <div class="row">
+                  <div class="col-12 q-pa-md">
+                    <q-avatar square>
+                      <img src="~assets/news.png" />
+                    </q-avatar>
+                    News 1
+                  </div>
+                  <div class="col-12 q-pa-md">
+                    <q-avatar square>
+                      <img src="~assets/news.png" />
+                    </q-avatar>
+                    News 2
+                  </div>
+                  <div class="col-12 q-pa-md">
+                    <q-avatar square>
+                      <img src="~assets/news.png" />
+                    </q-avatar>
+                    News 3
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+
+          <div class="col-12 col-md-1 text-transparent">_</div>
+
+          <div class="col-12 col-md-12 text-transparent">_</div>
+
+          <div class="col-12 col-md-1 text-transparent">_</div>
+
+          <!-- Calories Calculator -->
+
+          <div class="col-12 col-md-4">
+            <q-card>
+              <q-card-section class="bgmint text-weight-bolder text-white">
                 <q-avatar square>
-                  <img src="~assets/news.png" >
+                  <img src="~assets/calculator.png" />
                 </q-avatar>
-                  News 3
-              </div>
-            </div>
-          </q-card-section>
-        </q-card>
+                <span class="text-h5">Calories Calculator</span>
+              </q-card-section>
+              <q-separator class="bg-black" />
+              <q-card-section class="goal">
+                <q-form @reset="onReset">
+                  <div class="q-pa-md row" style="max-width: 400">
+                    <span>Protein:</span>
+                    <div class="q-gutter-md">
+                      <q-input v-model="calc.protein" type="number" />
+                    </div>
+                  </div>
+                  <div class="q-pa-md row" style="max-width: 400">
+                    <span>Carbohydrate: </span>
+                    <div class="q-gutter-md">
+                      <q-input v-model="calc.carbo" type="number" />
+                    </div>
+                  </div>
+                  <div class="q-pa-md row" style="max-width: 400">
+                    <span>Fat: </span>
+                    <div class="q-gutter-md">
+                      <q-input v-model="calc.fat" type="number" />
+                    </div>
+                  </div>
+                  <q-card-actions align="right">
+                    <q-btn
+                      label="Calculate"
+                      type="submit"
+                      class="bg-secondary text-white"
+                      @click="calculate"
+                    />
+
+                    <q-btn
+                      label="Reset"
+                      type="reset"
+                      text-color="red"
+                      class="q-ml-sm"
+                      flat
+                    />
+                  </q-card-actions>
+                </q-form>
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-12 col-md-1 text-transparent">_</div>
+
+          <!-- Health Record -->
+
+          <div class="col-12 col-md-5">
+            <q-card>
+              <q-card-section class="bgmint text-weight-bolder text-white">
+                <q-avatar square>
+                  <img src="~assets/health-report.png" />
+                </q-avatar>
+                <span class="text-h5">Health Record</span>
+              </q-card-section>
+              <q-separator class="bg-black" />
+              <q-card-section vertical class="goal">
+                <div class="row">
+                  <div class="col-12 q-pa-md">
+                    <q-avatar square>
+                      <img src="~assets/height.png" />
+                    </q-avatar>
+                    Height (cm):
+                  </div>
+                  <div class="col-12 q-pa-md">
+                    <q-avatar square>
+                      <img src="~assets/weight.png" />
+                    </q-avatar>
+                    Weight (kg):
+                  </div>
+                  <div class="col-12 q-pa-md">
+                    <q-avatar square>
+                      <img src="~assets/bmi.png" />
+                    </q-avatar>
+                    BMI:
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+
+          <div class="col-12 col-md-1 text-transparent">_</div>
         </div>
-
-        <div class="col-12 col-md-1 text-transparent">
-          _
-        </div>
-
-        <div class="col-12 col-md-12 text-transparent">
-          _
-        </div>
-
-        <div class="col-12 col-md-1 text-transparent">
-          _
-        </div>
-
-        <!-- Calories Calculator -->
-
-        <div class="col-12 col-md-4">
+      </q-card>
+      <q-dialog v-model="calories" persistent>
+        <div>
           <q-card>
-            <q-card-section class="bgmint text-weight-bolder text-white">
-              <q-avatar square>
-                <img src="~assets/calculator.png" >
-              </q-avatar>
-              <span class="text-h5 ">Calories Calculator</span>
+            <q-card-section class="bg-secondary">
+              <span class="text-white">Total Calories: {{ calc.total }}</span>
             </q-card-section>
-            <q-separator class="bg-black" />
-            <q-card-section class="goal">
-              <q-form
-                @reset="onReset"
-                >
-                <div class="q-pa-md row" style="max-width: 400">
-                  <span>Protein:</span>
-                  <div class="q-gutter-md">
-                    <q-input v-model="calc.protein" type="number"/>
-                  </div>
-                </div>
-                <div class="q-pa-md row" style="max-width: 400">
-                  <span>Carbohydrate: </span>
-                  <div class="q-gutter-md">
-                    <q-input v-model="calc.carbo" type="number"/>
-                  </div>
-                </div>
-                <div class="q-pa-md row" style="max-width: 400">
-                  <span>Fat: </span>
-                  <div class="q-gutter-md">
-                    <q-input v-model="calc.fat" type="number"/>
-                  </div>
-                </div>
-                <q-card-actions align="right">
-                  <q-btn
-                    label="Calculate"
-                    type="submit"
-                    class="bg-secondary text-white"
-                    @click="calculate"
-                  />
-
-                  <q-btn
-                    label="Reset"
-                    type="reset"
-                    text-color="red"
-                    class="q-ml-sm"
-                    flat
-                  />
-                </q-card-actions>
-              </q-form>
+            <q-separator />
+            <q-card-section class="bg-primary" align="right">
+              <q-btn
+                class="text-weight-bold"
+                rounded
+                flat
+                label="Close"
+                color="red"
+                v-close-popup
+              />
             </q-card-section>
           </q-card>
         </div>
-        <div class="col-12 col-md-1 text-transparent">
-          _
-        </div>
+      </q-dialog>
 
-        <!-- Health Record -->
-
-        <div class="col-12 col-md-5">
-          <q-card>
-          <q-card-section class="bgmint text-weight-bolder text-white">
-            <q-avatar square>
-              <img src="~assets/health-report.png" >
-            </q-avatar>
-            <span class="text-h5 ">Health Record</span>
+      <q-dialog v-model="submitGoal">
+        <q-card>
+          <q-card-section class="bg-secondary text-white">
+            Are you sure you want to submit ?
           </q-card-section>
-          <q-separator class="bg-black" />
-          <q-card-section vertical class="goal">
-            <div class="row">
-              <div class="col-12 q-pa-md">
-                <q-avatar square>
-                  <img src="~assets/height.png" >
-                </q-avatar>
-                  Height:
-              </div>
-              <div class="col-12 q-pa-md">
-                <q-avatar square>
-                  <img src="~assets/weight.png" >
-                </q-avatar>
-                  Weight:
-              </div>
-              <div class="col-12 q-pa-md">
-                <q-avatar square>
-                  <img src="~assets/bmi.png" >
-                </q-avatar>
-                  BMI:
-              </div>
-            </div>
+          <q-separator class="bg-grey" />
+          <q-card-section align="right" class="bg-primary text-white">
+            <q-btn label="Yes" class="bg-green" v-close-popup />
+            <q-btn label="No" class="text-red" flat v-close-popup />
           </q-card-section>
         </q-card>
-        </div>
-
-        <div class="col-12 col-md-1 text-transparent">
-          _
-        </div>
-
-      </div>
-    </q-card>
-    <q-dialog v-model="calories" persistent>
-      <div>
-      <q-card>
-        <q-card-section class="bg-secondary">
-          <span class="text-white">Total Calories: {{ calc.total }}</span>
-        </q-card-section>
-        <q-separator />
-        <q-card-section class="bg-primary" align="right">
-        <q-btn
-              class="text-weight-bold"
-              rounded
-              flat
-              label="Close"
-              color="red"
-              v-close-popup
-            />
-        </q-card-section>
-      </q-card>
-      </div>
-    </q-dialog>
-
-    <q-dialog v-model="submitGoal">
-      <q-card>
-        <q-card-section class="bg-secondary text-white">
-          Are you sure you want to submit ?
-        </q-card-section>
-        <q-separator class="bg-grey" />
-        <q-card-section align="right" class="bg-primary text-white">
-          <q-btn
-            label="Yes"
-            class="bg-green"
-            v-close-popup
-          />
-          <q-btn
-            label="No"
-            class="text-red"
-            flat
-            v-close-popup
-          />
-        </q-card-section>
-      </q-card>
-    </q-dialog>
-  </div>
+      </q-dialog>
+    </div>
   </q-page>
 </template>
 
@@ -267,23 +243,23 @@ export default {
     const store = useStore();
     const router = useRouter();
 
-    const goal1 = ref(false)
-    const goal2 = ref(false)
-    const goal3 = ref(false)
-    const calories = ref(false)
-    const calculator = ref(null)
-    const total = ref(null)
+    const goal1 = ref(false);
+    const goal2 = ref(false);
+    const goal3 = ref(false);
+    const calories = ref(false);
+    const calculator = ref(null);
+    const total = ref(null);
     const submitGoal = ref(false);
 
-    const calc = ref ({
+    const calc = ref({
       fat: 0,
       carbo: 0,
       protein: 0,
       calo: 0,
       total: "",
-    })
+    });
 
-    function calculate (){
+    function calculate() {
       const total = calc.value.fat + calc.value.protein + calc.value.carbo;
       calories.value = true;
       console.log(total);
@@ -302,39 +278,39 @@ export default {
       onReset() {
         calc.value = {};
       },
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style>
-  .title {
-    color: transparent;
-  }
+.title {
+  color: transparent;
+}
 
-  .background {
+.background {
   position: fixed;
   height: 100%;
   width: 100%;
   left: 0;
   top: 0;
-  }
+}
 
-  .mint {
-    /* color : #3eb489; */
-    color : #3eb489;
-  }
+.mint {
+  /* color : #3eb489; */
+  color: #3eb489;
+}
 
-  .bgmint {
-    /* background-color : rgb(211, 41, 41); */
-    background-color: #0a7950;
-  }
+.bgmint {
+  /* background-color : rgb(211, 41, 41); */
+  background-color: #0a7950;
+}
 
-  .background {
-    background-color: white;
-  }
+.background {
+  background-color: white;
+}
 
-  .goal {
-    background-color: #6ee0b6;
-  }
+.goal {
+  background-color: #6ee0b6;
+}
 </style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -3,68 +3,84 @@
     <img />
     <div class="row">
       <div class="col-12 col-md-12 text-transparent">
-        <q-card flat class="bg-grey-1">
-        -
-        </q-card>
+        <q-card flat class="bg-grey-1"> - </q-card>
       </div>
       <div class="col-12 col-md-12">
         <div class="content-center flex justify-center">
-        <q-card align="center">
-          <q-card-section align="center" class="bg-secondary">
-            <q-avatar square class="logo">
-              <img src="~assets/HLG_green.png" />
-            </q-avatar>
-            <span class="text-h3 text-weight-bolder login">
-              LOG IN
-            </span>
-          </q-card-section>
-          <q-separator class="bg-grey"/>
-          <q-card-section class="bg-primary">
-            <q-card class="bg-transparent" flat>
-            <q-card-section>
-              <q-form
-                @submit="confirm == true"
-                @reset="onReset"
-                class="q-gutter-md"
-              >
-                  <q-input v-model="login.username" label="Username">
-                    <template v-slot:before>
+          <q-card align="center">
+            <q-card-section align="center" class="bg-secondary">
+              <q-avatar square class="logo">
+                <img src="~assets/HLG_green.png" />
+              </q-avatar>
+              <span class="text-h3 text-weight-bolder login"> LOG IN </span>
+            </q-card-section>
+            <q-separator class="bg-grey" />
+            <q-card-section class="bg-primary">
+              <q-card class="bg-transparent" flat>
+                <q-card-section>
+                  <q-form
+                    @submit="confirm == true"
+                    @reset="onReset"
+                    class="q-gutter-md"
+                  >
+                    <q-input v-model="login.username" label="Username">
+                      <template v-slot:before>
                         <q-icon name="person" color="black" />
                       </template>
-                  </q-input>
-                  <q-input v-model="login.password" label="Password" :type="passwordVisibility ? 'text' : 'password'">
-                    <template v-slot:before>
-                      <q-icon name="key" color="black" />
-                    </template>
-                    <template v-slot:append>
-                    <q-icon
-                      name="remove_red_eye"
-                      color="grey"
-                      @click="togglePasswordVisibility"
-                      :class="passwordVisibility ? 'opac' : ''"
+                    </q-input>
+                    <q-input
+                      v-model="login.password"
+                      label="Password"
+                      :type="passwordVisibility ? 'text' : 'password'"
+                    >
+                      <template v-slot:before>
+                        <q-icon name="key" color="black" />
+                      </template>
+                      <template v-slot:append>
+                        <q-icon
+                          name="remove_red_eye"
+                          color="grey"
+                          @click="togglePasswordVisibility"
+                          :class="passwordVisibility ? 'opac' : ''"
+                        />
+                      </template>
+                    </q-input>
+                    <q-separator class="bg-transparent" />
+                    <q-btn
+                      color="green"
+                      text-color="white"
+                      label="Log In"
+                      @click="mainpage()"
                     />
-                  </template>
-                  </q-input>
-                  <q-separator class="bg-transparent"/>
-                    <q-btn color="green" text-color="white" label="Log In" @click="mainpage()"/>
-                    <q-btn text-color="red" label="Reset" flat @click="onReset()"/>
-                      <q-separator />
+                    <q-btn
+                      text-color="red"
+                      label="Reset"
+                      flat
+                      @click="onReset()"
+                    />
+                    <q-separator />
                     <q-btn flat @click="register()">
                       I don't have an account
                     </q-btn>
-              </q-form>
+                  </q-form>
+                </q-card-section>
+              </q-card>
             </q-card-section>
-            </q-card>
-          </q-card-section>
-        </q-card>
+          </q-card>
         </div>
       </div>
       <div class="col-12 col-md-12 text-transparent">
-        <q-card flat class="bg-grey-1">
-        -
-        </q-card>
+        <q-card flat class="bg-grey-1"> - </q-card>
       </div>
     </div>
+    <q-dialog v-model="error" persistent>
+      <q-card>
+        <q-card-section> Username or password are incorrect </q-card-section>
+        <q-card-actions align="right">
+          <q-btn text-color="red" label="Close" flat v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -80,6 +96,7 @@ export default {
     const $q = useQuasar();
     const store = useStore();
     const router = useRouter();
+    const error = ref(false);
 
     const columns = [...colUser];
     const rows = [...rowsUser];
@@ -95,13 +112,14 @@ export default {
       passwordVisibility.value = !passwordVisibility.value;
     }
 
-    return{
+    return {
       togglePasswordVisibility,
       passwordVisibility,
       confirm,
       columns,
       rows,
       login,
+      error,
       mainpage() {
         for (let i = 0; i < rows.length; i++) {
           const element = rows[i];
@@ -118,52 +136,53 @@ export default {
               localStorage.setItem("roleId", element.roleId);
               const roleId = element.roleId;
               if (roleId == 1) {
-                console.log(roleId)
+                console.log(roleId);
                 console.log("Admin");
                 router.push({ name: "mainpageAdmin" });
               } else if (roleId == 2) {
-                console.log(roleId)
+                console.log(roleId);
                 console.log("User");
                 router.push({ name: "mainpage" });
               }
             }
           } else {
-            console.log("x wujud");
+            console.log("xdop");
+            error.value = true;
           }
         }
       },
-      register (){
-        router.push({ name: "register"})
+      register() {
+        router.push({ name: "register" });
       },
 
       onReset() {
         login.value = {};
       },
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style>
-  .bground {
+.bground {
   position: fixed;
   height: 100%;
   width: 100%;
   left: 0;
   top: 0;
-  }
+}
 
-  .space {
-    padding-top: 5em;
-  }
+.space {
+  padding-top: 5em;
+}
 
-  .logo {
-    height: 75px;
-    width: 100px;
-  }
+.logo {
+  height: 75px;
+  width: 100px;
+}
 
-  .login {
-    color: #ffd700;
-    /* padding-top: em; */
-  }
+.login {
+  color: #ffd700;
+  /* padding-top: em; */
+}
 </style>
